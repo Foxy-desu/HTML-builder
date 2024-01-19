38057e427f5ce33174a4368f9450631107f137b5
Alexandra <iammeduza@gmail.com>
2024-01-20 00:33:31 +0300

    feat: implement a solution for the 2nd task

---
+++ 02-write-file/index.js
@@ -0,0 +1,47 @@
+const { stdin, stdout, exit } = process;
+const path = require('path');
+const fs = require('fs');
+
+const fileName = 'text.txt';
+
+function write(output, data) {
+  output.write(data.toString());
+}
+function sayBye() {
+  write(stdout, `\nYour ${fileName} was created successfully, goodbye.`);
+}
+function checkFolder(fileName) {
+  const filePath = path.join(__dirname);
+
+  fs.readdir(filePath, (err, data) => {
+    if (err) throw err;
+    return data.toString().includes(fileName);
+  });
+}
+function createFile(fileName) {
+  const hasFile = checkFolder(fileName);
+
+  if (!hasFile) {
+    fs.open(fileName, 'w', (err) => {
+      if (err) throw err;
+    });
+  }
+  const filePath = path.join(__dirname, fileName);
+  const output = fs.createWriteStream(filePath);
+  const beforeInputMsg =
+    '\nPlease, type any text and press "Enter" to add it to the file.\nPress "ctrl + c" or type "exit" to quit.\n';
+
+  stdout.write(beforeInputMsg);
+  stdin.on('data', (chunk) => {
+    const string = chunk.toString();
+    if (!(string.toLowerCase() === 'exit\r\n')) {
+      write(output, string);
+    } else {
+      exit();
+    }
+  });
+  process.on('exit', () => sayBye());
+  process.on('SIGINT', () => exit());
+}
+
+createFile(fileName);
