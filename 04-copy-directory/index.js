5e4047b4e28cfb8bb7ccbe9f975de980638b90df
Alexandra <iammeduza@gmail.com>
2024-01-21 23:36:12 +0300

    refactor: lint and format code

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,23 +2,22 @@ const fs = require('fs');
 const path = require('path');
 
 function makeCopy() {
-    const sourceDir = path.join(__dirname, "files");
-    const destDir = sourceDir + '-copy';
-    fs.rm(destDir, {recursive: true}, (err) => {
-        if (err) {};
-        fs.mkdir(destDir, {recursive: true},(err) => {
-            if(err) console.log('mkdir error');
-            fs.readdir(sourceDir, {withFileTypes: true},(err, files) => {
-                if(err) console.log('readdir error');
-                files.forEach((file) => {
-                    const dest = path.join(destDir, file.name);
-                    const src = path.join(file.path, file.name);
-                    fs.copyFile(src, dest, (err) => {
-                        if(err) console.log(err)
-                    });
-                })
-            })
-        })
-});
-    
-}makeCopy()
+  const sourceDir = path.join(__dirname, 'files');
+  const destDir = sourceDir + '-copy';
+  fs.rm(destDir, { recursive: true }, () => {
+    fs.mkdir(destDir, { recursive: true }, (err) => {
+      if (err) console.log('mkdir error');
+      fs.readdir(sourceDir, { withFileTypes: true }, (err, files) => {
+        if (err) console.log('readdir error');
+        files.forEach((file) => {
+          const dest = path.join(destDir, file.name);
+          const src = path.join(file.path, file.name);
+          fs.copyFile(src, dest, (err) => {
+            if (err) console.log(err);
+          });
+        });
+      });
+    });
+  });
+}
+makeCopy();
