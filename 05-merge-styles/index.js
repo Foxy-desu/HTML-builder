74daa8ff5e4cda2b7b4e8b69ac36d90fdd08daa1
Alexandra <iammeduza@gmail.com>
2024-01-23 18:58:50 +0300

    refactor: improve code quality using linter

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,33 +4,31 @@ const destDir = path.join(__dirname, 'project-dist');
 const sourceDir = path.join(__dirname, 'styles');
 
 function createBundle(source, dest) {
+  fs.readdir(source, { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
 
-    fs.readdir(source, {withFileTypes: true}, (err, files) => {
+    const arr = [];
+    files.forEach((entry) => {
+      const isFile = entry.isFile();
+      const fileExt = path.extname(entry.name);
+
+      if (isFile && fileExt === '.css') {
+        arr.push(entry);
+      }
+    });
+    arr.forEach((file) => {
+      const filePath = path.join(source, file.name);
+      fs.readFile(filePath, 'utf-8', (err, data) => {
         if (err) throw err;
-
-        const arr = [];
-        files.forEach((entry) => {
-            const isFile = entry.isFile();
-            const fileExt = path.extname(entry.name);
-
-            if(isFile && fileExt === '.css') {
-                arr.push(entry);
-            }
-        })
-        arr.forEach((file) => {
-            const filePath = path.join(source, file.name);
-            fs.readFile(filePath, 'utf-8', (err, data) => {
-                if(err) throw err;
-                const fileName = 'bundle.css';
-                const fileP = path.join(dest, fileName);
-                fs.unlink(fileP, () => {
-                    fs.appendFile(fileP, data, (err) => {
-                        if (err) throw err;
-                    });
-                });
-            })
-        })
-
-    })
-};
+        const fileName = 'bundle.css';
+        const fileP = path.join(dest, fileName);
+        fs.unlink(fileP, () => {
+          fs.appendFile(fileP, data, (err) => {
+            if (err) throw err;
+          });
+        });
+      });
+    });
+  });
+}
 createBundle(sourceDir, destDir);
